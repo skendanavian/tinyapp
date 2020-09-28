c3279b91dbda334597c56e6c98ae9f86916dea3e
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-28 20:28:39 +0000

    add random 6 character id function, implement post functionality to receive long URL

--- express_server.js
+++ express_server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  const randomString = Math.random().toString(32).substring(2, 8);
+  return randomString;
+}
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -21,15 +29,27 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
 
 
 app.listen(PORT, () => {
