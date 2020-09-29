e361b587ef7f1c0dbe5fa22ab0b76bd8837a8adf
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-29 21:18:20 +0000

    added functionality to username cookie info. Displays Username in navbar of all pages

--- express_server.js
+++ express_server.js
@@ -41,16 +41,17 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {username: req.cookies["username"], urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   if (templateVars.longURL) {
     res.render("urls_show", templateVars);
   } else {
